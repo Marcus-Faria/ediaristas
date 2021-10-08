7ead6960addef40cb16ac1d64db8dcd3ad1f3a51
Marcus-Faria <marcusviniciusfaria150@gmail.com>
2021-10-08 11:18:10 -0300

    Update ediaristas

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,8 @@
 /*Criando atalho para acessar caminho style*/
-import '@styles/globals.css' 
-import type { AppProps } from 'next/app'
+import "@styles/globals.css";
+import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
-export default MyApp
+export default MyApp;
